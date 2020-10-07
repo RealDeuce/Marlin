940889acd2ce906620a62cf104d563cd53ef9dfd
Stephen Hurd <shurd@sasktel.net>
2020-10-07 05:02:20 -0400

    Change Z home position to as close to the front-left screw as possible. This location is where the assisted tramming references, and is the minimum X position... so a crooked X gantry will have the smallest possible impact on Z probe offset.
    Slightly lower Junction Deviation.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -823,7 +823,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.08 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.075 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
@@ -1417,8 +1417,8 @@
 #endif
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT 47 // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT 47 // Y point for Z homing
 #endif
 
 // Homing speeds (mm/min)
