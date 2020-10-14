847c30dfcfa3e91b022f6f7564b693e740a0c373
Stephen Hurd <shurd@sasktel.net>
2020-10-14 04:52:19 -0400

    Disable S-curve accel since Lin Advance is better(?)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -836,7 +836,8 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+// NOTE: Incompatible with Linear Advance...
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
