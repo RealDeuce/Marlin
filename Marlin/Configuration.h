9ee18bf1cdec6bb28b63be92e2e26ae3b7696633
Stephen Hurd <shurd@sasktel.net>
2020-10-14 05:54:28 -0400

    Set E jerk to 30 for Linear Advance as a work-around for the 2208 Linear Advance StealthChop problem (Bug #16076).

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -813,7 +813,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    30.0  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
