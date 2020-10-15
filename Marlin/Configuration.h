20f01072bd07506a3ae5a04fdae7ef64b7d570e5
Stephen Hurd <shurd@sasktel.net>
2020-10-14 21:13:17 -0400

    Add comment asserting that s-curve causes E-Stall (untested)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -837,6 +837,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
+// Enabling this triggers E-Stall at high print/retraction speeds.
 //#define S_CURVE_ACCELERATION
 
 //===========================================================================
