d70d1cb2c948e228ae5d8ab114422d437069d730
Stephen Hurd <shurd@sasktel.net>
2020-11-10 18:42:07 -0500

    Set default max accel to experiment best values.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -763,7 +763,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 8000, 4000, 100, 800 }
+#define DEFAULT_MAX_ACCELERATION      { 7000, 2000, 100, 800 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -778,9 +778,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          8000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          7000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION   800    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   8000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   7000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
